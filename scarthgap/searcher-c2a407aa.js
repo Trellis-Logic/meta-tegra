5f32b12448fd53a4a6e711d5269c0cfab11bdee9
dwalkes <dwalkes@users.noreply.github.com>
2026-02-08 14:23:32 +0000

    deploy: 2faf2d3c577bb15b7e6619b8e247a4786d954555

--- scarthgap/searcher-c2a407aa.js
+++ scarthgap/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a516ad41.js',
+                path_to_root + 'searchindex-7c590dfa.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
